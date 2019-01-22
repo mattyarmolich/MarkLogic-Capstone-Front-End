8faef04feca0328f2c8bd54ed0f97e67e861865f
Matt Yarmolich <mattyarmo@gmail.com>
2019-01-22 10:30:31 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
+import CreateAccount from './Pages/CreateAccount/CreateAccount';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.getUsers();
+  }
+  //http://54.212.203.52:5001/users/register
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { console.log(res); })
+    .catch((err) => { console.log(err); });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Route path="/" exact component={Login} />
+            <Route path="/Home/" component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+          </header>
+        </div>
+
+      </Router>
+
+
+    );
+  }
+}
+
+export default App;
