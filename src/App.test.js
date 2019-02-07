dc967e3b58a81e201ced13905f122051f6b9116f
Matt Yarmolich <mattyarmo@gmail.com>
2019-02-07 11:44:09 -0800

    added first running test :D

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Uploads from "./pages/UploadsPage/Uploads";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Uploads />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
