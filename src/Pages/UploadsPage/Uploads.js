eabcb0e95705964ce41ed796d95e7822367a967a
Matt Yarmolich <mattyarmo@gmail.com>
2019-02-07 11:40:53 -0800

    added enzyme, no tests pass though

--- src/Pages/UploadsPage/Uploads.js
+++ src/Pages/UploadsPage/Uploads.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
-import './Uploads.scss';
-import axios from 'axios';
-import DragDrop from './../../Components/DragDrop/DragDrop';
-import Modal from 'react-modal';
+import React, { Component } from "react";
+import "./Uploads.scss";
+import axios from "axios";
+import DragDrop from "./../../Components/DragDrop/DragDrop";
+import Modal from "react-modal";
 
-Modal.setAppElement('#root');
+if (process.env.NODE_ENV !== "test") {
+  Modal.setAppElement("#root");
+}
 
 class Uploads extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       showModal: false,
-      files: [
-        "file0", "file1", "file2"
-      ]
+      files: ["file0", "file1", "file2"]
     };
 
     this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
-   handleOpenModal = () => {
-     console.log("this ran")
-     this.setState({ showModal: true });
-   }
+  handleOpenModal = () => {
+    console.log("this ran");
+    this.setState({ showModal: true });
+  };
 
-   handleCloseModal = () => {
-     this.setState({ showModal: false });
-   }
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
 
-
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   render() {
     return (
@@ -40,33 +37,37 @@ class Uploads extends Component {
           <div className="Title-text">Files</div>
           <div className="current-files">
             <ul className="current-files">
-            {
-              this.state.files.map((item,i) => <li key={i}>{item}</li>)
-            }
+              {this.state.files.map((item, i) => (
+                <li key={i}>{item}</li>
+              ))}
             </ul>
-
           </div>
           <div className="bottom-part">
-            <button className="upload-button" onClick={() => this.handleOpenModal()}>Upload New File</button>
-            <button className="next" onClick={this.props.nextStep}>next</button>
+            <button
+              className="upload-button"
+              onClick={() => this.handleOpenModal()}
+            >
+              Upload New File
+            </button>
+            <button className="next" onClick={this.props.nextStep}>
+              next
+            </button>
           </div>
 
-            <Modal
-              isOpen={this.state.showModal}
-              onAfterOpen={this.afterOpenModal}
-              onRequestClose={this.closeModal}
-              className="upload-file-modal"
-              contentLabel="Example Modal"
-            >
+          <Modal
+            isOpen={this.state.showModal}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            className="upload-file-modal"
+            contentLabel="Example Modal"
+          >
             <button onClick={() => this.handleCloseModal()}>close</button>
 
             <DragDrop />
-            </Modal>
-
+          </Modal>
         </div>
       </div>
-
-    )
+    );
   }
 }
 
