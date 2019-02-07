e6cb32e3a374c640a3b1ff8fb9e63cb4e221978b
Matt Yarmolich <mattyarmo@gmail.com>
2019-02-07 11:47:22 -0800

    added another test

--- src/shallow.Test.js
+++ src/shallow.Test.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-const mockStore = configureMockStore();
-const store = mockStore({});
 it("renders without crashing", () => {
-  shallow(<App store={store} />);
+  shallow(<App />);
 });
