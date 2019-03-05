f24d44b8f46fbc3d2cfb00b434c50d8f856684e6
Matt Yarmolich <mattyarmo@gmail.com>
2019-03-05 13:09:03 -0800

    added some extra features for free

--- src/Pages/ExportPage/ExportPage.js
+++ src/Pages/ExportPage/ExportPage.js
@@ -14,7 +14,12 @@ class ExportPage extends Component {
       <div className="loading-container">
         <div className="loading-view">
           <div className="Title-text">Export</div>
+          <div>
+            <div>Download Classified Data</div>
+            <button>Download</button>
+          </div>
           <div className="bottom-part">
+            <button onClick={this.props.firstStep}>Back to Beginning</button>
             <button onClick={this.props.previousStep}>previous</button>
           </div>
         </div>
