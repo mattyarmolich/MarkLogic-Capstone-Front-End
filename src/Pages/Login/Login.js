198cfb5bc29b6de89223817ed89e2b549bf867ac
landongerrits <landon.gerrits@me.com>
2019-04-17 23:24:23 -0700

    first commit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,9 +7,55 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as authActions from "../../utils/actions/authActions";
 import { Switch, Route, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 //React router imports
 import { Link } from "react-router-dom";
+
+const styles = theme => ({
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
 
 class Login extends Component {
   constructor(props) {
@@ -47,40 +93,40 @@ class Login extends Component {
     this.props.authActions.fetchAuth(this.state.email, this.state.password);
   };
 
-  render() {
-    return (
-      <div className="login-container">
-        <img className="logo" src={logo} />
+  // render() {
+  //   return (
+  //     <div className="login-container">
+  //       <img className="logo" src={logo} />
 
-        <input
-          id="email"
-          placeholder="Email"
-          className="login"
-          onChange={e => this.onChange(e)}
-        />
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          className="login"
-          onChange={e => this.onChange(e)}
-        />
+  //       <input
+  //         id="email"
+  //         placeholder="Email"
+  //         className="login"
+  //         onChange={e => this.onChange(e)}
+  //       />
+  //       <input
+  //         type="password"
+  //         id="password"
+  //         placeholder="Password"
+  //         className="login"
+  //         onChange={e => this.onChange(e)}
+  //       />
 
-        <button to="/Home" className="submit" onClick={() => this.onSubmit()}>
-          <div className="login-text">Submit</div>
-        </button>
+  //       <button to="/Home" className="submit" onClick={() => this.onSubmit()}>
+  //         <div className="login-text">Submit</div>
+  //       </button>
 
-        <div className="footer">
-          {"Dont have an account? "}
-          <Link to="/CreateAccount">Sign Up</Link>
-        </div>
-        <div className="footer">
-          {"Forgot your password? "}
-          <Link to="/ResetPassword">Link</Link>
-        </div>
-      </div>
-    );
-  }
+  //       <div className="footer">
+  //         {"Dont have an account? "}
+  //         <Link to="/CreateAccount">Sign Up</Link>
+  //       </div>
+  //       <div className="footer">
+  //         {"Forgot your password? "}
+  //         <Link to="/ResetPassword">Link</Link>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 }
 
 function mapStateToProps(state) {
@@ -95,7 +141,59 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
+function SignIn(props) {
+  const { classes } = props;
+
+  return (
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => this.onSubmit()}
+          >
+            Sign in
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
+}
+
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
 export default withRouter(
+  withStyles(styles)(SignIn),
   connect(
     mapStateToProps,
     mapDispatchToProps
